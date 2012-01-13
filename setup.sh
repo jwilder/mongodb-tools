45b8bcc8fe3633e3dd87b0081ed9ce0332940e83
Jason Wilder <jwilder@litl.com>
2012-01-13 16:47:13 -0700

    Add a simple setup.sh script
    Setups a virtualenv and installs dependencies defined in requirements.txt

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VIRTUALENV_INSTALLED=`which virtualenv`
+if [ "$VIRTUALENV_INSTALLED" == "" ]; then
+    echo -e "virtualenv NOT found."
+    exit 1
+fi
+
+virtualenv --no-site-packages virtualenv
+source virtualenv/bin/activate
+pip install -r requirements.txt
+
+echo "Run \"source virtualenv/bin/activate\" to activate this virtualenv."
+
